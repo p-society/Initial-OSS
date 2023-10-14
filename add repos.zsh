49268ea2761df544d7ca346ed1284842fb2b41d3
Soubhik <119437069+zakhaev26@users.noreply.github.com>
2023-10-14 19:47:19 +0000

    add l3

--- add repos.zsh
+++
@@ -1,4 +0,0 @@
-git submodule add https://github.com/zakhaev26/Task-Tracker-App.git webl2:ToDoList
-git submodule add https://github.com/zakhaev26/OiDE-compiler.git webl3:OnlineIDE
-git submodule add https://github.com/zakhaev26/Spotify-Clone.git webl1:SpotifyClone
-
---
+++ add-repos.zsh
@@ -0,0 +1,5 @@
+# git submodule add https://github.com/zakhaev26/Task-Tracker-App.git webl2:ToDoList
+# git submodule add https://github.com/zakhaev26/OiDE-compiler.git webl3:OnlineIDE
+# git submodule add https://github.com/zakhaev26/Spotify-Clone.git webl1:SpotifyClone
+
+git submodule add https://github.com/majorbruteforce/Tunescape.git webl3:Tunescape
