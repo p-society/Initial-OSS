48dc7fd2577c5823834a1ad385fba31740316e3b
Soubhik Gon <119437069+zakhaev26@users.noreply.github.com>
2023-11-19 23:56:30 +0530

    Create B422056.go

---
+++ Git-Github-Session/B422056.go
@@ -0,0 +1,15 @@
+//this code prints hello world :D
+
+package main
+
+import "fmt"
+
+func printText() {
+  fmt.Println("Hello World ;) ");
+}
+
+func main() {
+    for i:=0;i<69;i++ {
+      printText();
+    }
+}
